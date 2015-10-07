f8e535f2f399bba50405f83fca92bc19eb6b321e
David Frank <info@frankdavid.hu>
2015-10-07 10:36:19 +0200

    Clean up and solve race condition in the Crawler.
    Added a inProgress set to keep record of ongoing crawls.
    Handled race condition when polling from the queue.
    Changed vars into vals.
    Changed a few lines to be more Scalaish.
    Renamed a few variables to camel case.
    
    
    Former-commit-id: 16bcc8b06fd0292e702e4dc26caeb8bfb533366f [formerly ebabde0f551cb2601bb08abbccfb39ae464dc646]
    Former-commit-id: 8df004568f409fb16b921abb997563d2e939c412

--- src/Crawler.scala
+++ src/Crawler.scala
@@ -9,92 +9,93 @@ import scala.collection.parallel.mutable.ParHashMap
 
 object Crawler {
 
-  var seedUrl: String = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/"
-  var visitedUrls = new ConcurrentSkipListSet[String]()
-  var enquedUrls = new ConcurrentSkipListSet[String]()
-  var queue = new LinkedBlockingQueue[String]()
-  var linkContent = new ConcurrentHashMap[String, String]()
-
+  val seedUrl: String = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/"
+  val visitedUrls = new ConcurrentSkipListSet[String]()
+  val enqueuedUrls = new ConcurrentSkipListSet[String]()
+  val inProgress = new ConcurrentSkipListSet[String]()
+  val queue = new LinkedBlockingQueue[String]()
+  val linkContent = new ConcurrentHashMap[String, String]()
   val exceptionCount = new ParHashMap[String, Int]()
 
-  def filterURLList(url_list: List[String]): Set[String] = {
-
-    var url_list_normalized = url_list.map(x => x.replaceAll("#.*", ""))
-
-    //TODO investigate this lines influence on neighbbours
-    url_list_normalized = url_list_normalized.map(x => x.replaceAll("\\?.*", ""))
-
-    //sort unsupported ends
-    url_list_normalized = url_list_normalized.filter(x => x.matches(".*(\\.html?|\\/[^\\/\\.]*)$"))
-//    url_list_normalized = url_list_normalized.filter(x => (!x.endsWith(".png") &&
-//      !x.endsWith(".jpg") && !x.endsWith(".pdf") && !x.endsWith("doc") && !x.endsWith(".docx")))
-
-
-    //filter out url pointing to outside
-    return url_list_normalized.filter(x => x.startsWith(seedUrl)).toSet
+  def normalizeAndFilterURLList(urlList: List[String]): Set[String] = {
+    urlList
+        .map(x => x.replaceAll("#.*", ""))
+//        .map(x => x.replaceAll("\\?.*", "")) //TODO investigate this lines influence on neighbours
+        .filter(x => x.matches(".*(\\.html?|\\/[^\\/\\.]*)(\\?.*)?$")) //sort unsupported ends
+        .filter(x => x.startsWith(seedUrl)) //filter out url pointing to outside
+        .toSet
   }
 
   def getURLsFromDoc(doc: Document): Set[String] = {
-    var links = doc.select("a[href]").map(x => x.attr("abs:href"))
-    return filterURLList(links.toList)
+    val links = doc.select("a[href]").map(x => x.attr("abs:href"))
+    normalizeAndFilterURLList(links.toList)
   }
 
   def fetchDocumentFromURL(url: String): Document = {
-    return Jsoup.connect(url).timeout(5000).get()
-    //    return Jsoup.parse(Source.fromURL(url).mkString)
+    Jsoup.connect(url).timeout(5000).get()
   }
 
   def extractText(doc: Document): String = {
     if(doc.select("#contentMain").text.length > 100) {
-      return doc.select("#contentMain").text
+      doc.select("#contentMain").text
+    } else {
+      doc.text
     }
-    return doc.text
   }
 
-  class CrawlerThread(id: Int) extends Runnable {
+  class CrawlerThread(id: Int) extends Thread {
 
-    def run {
-      while (queue.nonEmpty) {
-        val url = queue.poll()
-        try {
-          var doc = fetchDocumentFromURL(url)
-          //println(id, visitedUrls.size, queue.size, url)
+    override def run(): Unit = {
+        val url = Crawler.this.synchronized {
+          val url = queue.poll()
+          if (url != null) {
+            inProgress.add(url)
+          }
+          url
+        }
+        if (url == null) {
+          if (inProgress.size() > 0) {
+            Thread.sleep(200)
+            run()
+          }
+        } else {
+          try {
+            val doc = fetchDocumentFromURL(url)
+            //println(id, visitedUrls.size, queue.size, url)
 
-          linkContent(url) = Normalizer.normalize(extractText(doc))
+            linkContent(url) = Normalizer.normalize(extractText(doc))
 
             visitedUrls += url
             if (visitedUrls.size % 100 == 0) {
               println(visitedUrls.size, queue.size)
             }
-          for (url <- getURLsFromDoc(doc).filter(x => !enquedUrls.contains(x))) {
+            for (url <- getURLsFromDoc(doc).filter(x => !enqueuedUrls.contains(x))) {
               queue.add(url)
-              enquedUrls += url
+              enqueuedUrls += url
+            }
           }
+          catch {
+            case e: org.jsoup.HttpStatusException =>
+              val message = "status" + e.getStatusCode
+              exceptionCount(message) = exceptionCount.getOrElse(message, 0) + 1
+            case e: Throwable =>
+              e.printStackTrace()
+              val message = e.getClass.getName + " " + e.getMessage
+              exceptionCount(message) = exceptionCount.getOrElse(message, 0) + 1
+          }
+          inProgress -= url
+          run()
         }
-        catch {
-          case e: org.jsoup.HttpStatusException =>
-            val message = "status" + e.getStatusCode
-            exceptionCount(message) = exceptionCount.getOrElse(message, 0) + 1
-          case e: Throwable =>
-            e.printStackTrace()
-            val message = e.getClass.getName + " " + e.getMessage
-            exceptionCount(message) = exceptionCount.getOrElse(message, 0) + 1
-        }
-
       }
-    }
-
   }
 
 
   def bfsFromSeed(seed: String): Unit = {
-
     queue.add(seed)
-    enquedUrls += seed
-    val threads = for(i <- 1 to 10) yield {
-      val t = new Thread(new CrawlerThread(i))
+    enqueuedUrls += seed
+    val threads = for(i <- 1 to 30) yield {
+      val t = new CrawlerThread(i)
       t.start()
-      Thread.sleep(1000)
       t
     }
     threads.foreach(_.join())
@@ -108,7 +109,7 @@ object Crawler {
 //    var a = fetchDocumentFromURL("http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/en/studies/non-degree-courses/exchange-and-visiting-studies/programmes.html")
 //    println(extractText(a).length, extractText(a))
 
-    var seed = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/en.html"
+    val seed = "http://idvm-infk-hofmann03.inf.ethz.ch/eth/www.ethz.ch/en.html"
     bfsFromSeed(seed)
     println(linkContent.size)
 
