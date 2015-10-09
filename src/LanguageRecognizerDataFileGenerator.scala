ce7da5601121663059de5290ba22fe6f40b11bea
David Frank <info@frankdavid.hu>
2015-10-09 21:22:27 +0200

    Fix file paths in language file generator.
    Former-commit-id: 5cf0b3227755d0826d48aeed874dc2bf54075b62 [formerly 188715263e42a54053a4cf33f097766aad352a39]
    Former-commit-id: dbd109ca743e930e9ea35bdf58559d9e27d7caa7

--- src/LanguageRecognizerDataFileGenerator.scala
+++ src/LanguageRecognizerDataFileGenerator.scala
@@ -1,15 +1,13 @@
-import java.io.{FileInputStream, File}
+import java.io.File
 import java.util.Locale
 
 object LanguageRecognizerDataFileGenerator {
 
   def main(args: Array[String]) {
-    val dir = "data/lang/"
+    LanguageScorer.trainFromText(Locale.ENGLISH, FileLoader.loadFileFromPathOrJar("data/1.en"), 3)
+        .writeToFile(new File("data/frequencies_en.dat"))
 
-    LanguageScorer.trainFromText(Locale.ENGLISH, new FileInputStream(new File(dir + "1.en")), 3)
-        .writeToFile(new File("frequencies_en.dat"))
-
-    LanguageScorer.trainFromText(Locale.GERMAN, new FileInputStream(new File(dir + "1.de")), 3)
-        .writeToFile(new File("frequencies_de.dat"))
+    LanguageScorer.trainFromText(Locale.GERMAN, FileLoader.loadFileFromPathOrJar("data/1.de"), 3)
+        .writeToFile(new File("data/frequencies_de.dat"))
   }
 }
